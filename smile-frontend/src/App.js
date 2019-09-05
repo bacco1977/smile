a52b7fb1311c3c263f77a2ad0695b05589693feb
Phoebe Staab <phoebestaab@gmail.com>
2019-09-05 10:03:03 +0100

    react router

--- smile-frontend/src/App.js
+++ smile-frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import SmileLanding from './components/SmileLanding';
+import {Route, HashRouter} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <SmileLanding></SmileLanding>
-    </div>
-  );
+    return (
+      <HashRouter basename="/">
+        <Route exact path="/" render={() => <SmileLanding />}/>
+      </HashRouter>
+    );
 }
 
 export default App;
