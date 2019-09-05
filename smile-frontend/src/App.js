d52df6603e9d844a6a41a9ea9bd30dcb3b101a01
Phoebe Staab <phoebestaab@gmail.com>
2019-09-05 09:25:29 +0100

    frontend boilerplate
    React boilerplate with bootstrap

---
+++ smile-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SmileLanding from './components/SmileLanding';
+
+function App() {
+  return (
+    <div>
+      <SmileLanding></SmileLanding>
+    </div>
+  );
+}
+
+export default App;
