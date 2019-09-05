2ffc4a69bf7e7884db22b9d4ba23723f6c22f066
Phoebe Staab <phoebestaab@gmail.com>
2019-09-05 10:35:22 +0100

    basic routing

--- smile-frontend/src/App.js
+++ smile-frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import SmileLanding from './components/SmileLanding';
 import {Route, HashRouter} from 'react-router-dom';
+import ImageUpload from './components/ImageUpload';
+import Navigation from './components/Navigation';
 
 function App() {
     return (
-      <HashRouter basename="/">
-        <Route exact path="/" render={() => <SmileLanding />}/>
-      </HashRouter>
+      <div className="container">
+        <Navigation/>
+        <HashRouter basename="/">
+          <Route exact path="/" render={() => <SmileLanding />}/>
+          <Route exact path="/upload" render={() => <ImageUpload />}/>
+        </HashRouter>
+      </div>
     );
 }
 
